1c2b09143abeaef4a70161523cd9e3065beca784
Mikhail Burdukovsky <burmspb@gmail.com>
2026-06-18 09:42:32 +0000

    fixes for filtering the main table

--- daily/views.py
+++ daily/views.py
@@ -10,17 +10,17 @@ class TaskListView(ListView):
         # Получаем базовый набор всех записей из PostgreSQL
         queryset = super().get_queryset()
         
-        # 1. Фильтрация по наименованию контрагента
+        # Фильтрация по наименованию контрагента
         name_query = self.request.GET.get('name', '').strip()
         if name_query:
             queryset = queryset.filter(name__icontains=name_query)
             
-        # 2. Фильтрация по флагу управления
+        # Фильтрация по флагу управления
         flag_query = self.request.GET.get('flag', '')
         if flag_query != '':
             queryset = queryset.filter(status_flag=int(flag_query))
             
-        # 3. Сортировка по времени создания или по умолчанию (PK)
+        # Сортировка по времени создания или по умолчанию (PK)
         sort_query = self.request.GET.get('sort', '')
         if sort_query == 'newest':
             queryset = queryset.order_by('-created_at', '-id')
@@ -38,3 +38,6 @@ class TaskListView(ListView):
         context['current_flag'] = self.request.GET.get('flag', '')
         context['current_sort'] = self.request.GET.get('sort', '')
         return context
+    
+
+
